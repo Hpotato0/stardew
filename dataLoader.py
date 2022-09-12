6aaefe4643fcfc4ce471980482cf7a609ba1d54c
Jae <jhju2000@gmail.com>
2022-09-12 20:32:54 +0900

    fixed bug in preprocessing migration

--- dataLoader.py
+++ dataLoader.py
@@ -3,7 +3,6 @@
 from glob import glob
 from tqdm import tqdm
 from pandasql import sqldf
-from sklearn.linear_model import LinearRegression, Ridge
 from datetime import datetime
 import pandas as pd
 import numpy as np
@@ -14,21 +13,21 @@ import os
 from utility_submit import *
 
 
-class dataLoader(object):
+class dataLoader:
     #@ dir: aT_train_raw/pummok_*.csv
     def __init__(self, dir, saveFilePrefix = "", type = "csv"):
-        self.data_list = glob(dir) 
         self.pummok_list = glob(dir) #@ list of pummok csv directories, in random order  
         self.pummokData = [None]*(37) #@ placeholder for each type's data, in correct order
         self.saveFilePrefix = saveFilePrefix
         self.type = type #@ what type of data to load
 
+    #@ unified load function
+    #@ data is loaded into self.pummokData
     def load(self, save = False, load = False):
         if self.type == "csv":
             self.loadFromCsv(save = save, load = load)
         elif self.type == "pptxt":
             self.loadFromPPrTxt()
-
 
     #@ load pummok data from csv file
     #@ save/load simplified data in ./{dirName}/pummok, dirName = "pp_data" by default
@@ -63,7 +62,7 @@ class dataLoader(object):
 
                 ddf.to_csv(savePath, index=False)
 
-    #@ load txt data created by joykang's preprocessor
+    #@ load txt data created by ejchung's preprocessor
     def loadFromPPrTxt(self):
         for filepath in tqdm(self.pummok_list):
             name = filepath.split('/')[-1]
@@ -71,15 +70,24 @@ class dataLoader(object):
                 price = np.loadtxt(pricefile)
             self.pummokData[int(name.split(".")[0])] = price
 
-    #@ fill single NaNs with the average of date before & after
-    #@ fill multiple NaNs with linear interpolation
-    def fillNaN(self):
+    #@ ejchung's preprocessing code migrated to class method
+    #@ i.e. fill NaNs & remove outliers via a median filter
+    #@ the preprocessing is done on self.pummokData
+    def ejsPreProcess(self):
         for tidx, ddf in enumerate(self.pummokData):
             price = ddf["해당일자_전체평균가격(원)"].to_numpy()
+            print(len(ddf.loc[:,"해당일자_전체평균가격(원)"]))  
+
+            #@ manual removal of outliers
+            if tidx==0:
+                ddf.loc[ddf["datadate"]=="20130102","해당일자_전체평균가격(원)"] = np.nan
+            if tidx == 1:
+                ddf.loc[ddf["datadate"]=="20140211","해당일자_전체평균가격(원)"] = np.nan
+                ddf.loc[ddf["datadate"]=="20150901","해당일자_전체평균가격(원)"] = np.nan
 
             #@ remove outliers using median filter
             nz = np.reshape(np.argwhere(~np.isnan(price)), (1,-1))[0]
-            price[nz] = ndimage.median_filter(price[nz], size = 5)
+            price[nz] = ndimage.median_filter(price[nz], size = 3)
             
             #@ fill single NaNs with the average of data before & after
             if np.isnan(price[0]) and not np.isnan(price[1]):
@@ -89,9 +97,14 @@ class dataLoader(object):
                     price[i] = (price[i-1] + price[i+1])/2
             if np.isnan(price[-1]) and not np.isnan(price[-2]):
                 price[-1] = price[-2]
-            
-            #@ fill multiple NaNs with linear interpolation
-            if 0:
+             
+            if 1:
+                #@ fill multiple NaNs with the nearest non-NaN value
+                nz = np.reshape(np.argwhere(~np.isnan(price)), (1,-1))[0]
+                price = [x if not np.isnan(x) else price[nz[np.argmin(np.abs(i-nz))]] for i, x in enumerate(price)]
+            else:
+                #@ fill multiple NaNs with linear interpolation
+                #@ doesn't work as well..?
                 start = 0
                 end = 0
                 interp = False
@@ -115,62 +128,21 @@ class dataLoader(object):
 
                         a = np.array([p_s + (p_e-p_s)/(end-start+1)*x for x in range(1,end-start+2)])
                         price[start:end+1] = np.array([p_s + (p_e-p_s)/(end-start+1)*x for x in range(1,end-start+2)])
-            else:
-                nz = np.reshape(np.argwhere(~np.isnan(price)), (1,-1))[0]
-                price = [x if not np.isnan(x) else price[nz[np.argmin(np.abs(i-nz))]] for i, x in enumerate(price)]
 
+            print(len(ddf.loc[:,"해당일자_전체평균가격(원)"]))    
             ddf.loc[:,"해당일자_전체평균가격(원)"] = price
 
 
 
-#@ maps date string in format "YYYYMMDD"
-#@ to an integer in [0,364] ([0,365] for leap years)
-def dateToDayLoc(dateStr):
-    dS = dateStr.strip()
-    date_format = "%Y%m%d"
-    i = datetime.strptime(dS[:4]+"0101", date_format)
-    f = datetime.strptime(dS, date_format)
-    return (f - i).days
 
 if __name__ == "__main__":
-    #@ load euijun's preprocessed data
-    trainData = dataLoader('./euijun/data/prices/*.txt', type = "pptxt")
-    trainData.load()
 
-    #@ load raw test data
-    testDataSet = []
-    for idx in range(0, 9 + 1):
-        testDataSet.append(dataLoader(f'./aT_test_raw/sep_{idx}/pummok_*.csv', saveFilePrefix = f"test{idx}_", type = "csv"))
-        testDataSet[idx].load(save = False, load = True)    #@ switch save & load arguments when in need
 
-    #@ load config file
-    config_module =	__import__('config')
-    cfg= config_module.config
-    ctitle = 'correlated'
+    
 
-    #@ average the correlated yearly data (those in config.py )
-    trends = []
-    for idx in range(0, 36 + 1):
-        splits = np.split(trainData.pummokData[idx], [365, 365*2, 365*3, 365*4])
-        splits.pop(-1)
+    
 
-        #@ print correlation matrix for each type
-        #print(f"\n{idx}")
-        #print(np.corrcoef([splits[0], splits[1], splits[2], splits[3]]))
-
-        if not cfg[ctitle][idx]:
-            trends.append(np.array([]))
-            continue
-        
-        relIdx = [j-1 for j in cfg[ctitle][idx]]
-        baseTrend = splits[relIdx[-1]]
-
-        linreg = [LinearRegression().fit(np.reshape(splits[j], (-1,1)), baseTrend) for j in relIdx]
-        avg = np.zeros(365)
-        for regIdx, j in enumerate(relIdx):
-            avg = avg + splits[j] * linreg[regIdx].coef_[0] + linreg[regIdx].intercept_
-        avg = avg / len(relIdx)
-        trends.append(avg)
+    
 
     #@ predict 
     allPriceAns = []
